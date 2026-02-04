090c2b2b23a23a80bb666e0b5278cf2e916cb87c
Aparna Singh <aparnasingh29062006@gmail.com>
2026-02-04 21:19:03 +0530

    Add function comment for removeElement
    Add a comment explaining the removeElement function.

---
+++ Day 4/Question 2.c
@@ -0,0 +1,15 @@
+/*Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.*/
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+    int i;
+
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
