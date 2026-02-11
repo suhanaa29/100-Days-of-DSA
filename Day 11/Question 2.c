c2cb3d988409bac472cf7e9083bb50a64f7d9598
Aparna Singh <aparnasingh29062006@gmail.com>
2026-02-11 08:58:48 +0530

    Create Question 2.c

---
+++ Day 11/Question 2.c
@@ -0,0 +1,29 @@
+/*Given a 2D integer array matrix, return the transpose of matrix.*/
+
+#include <stdlib.h>
+
+int** transpose(int** matrix, int matrixSize, int* matrixColSize,
+                int* returnSize, int** returnColumnSizes) {
+    
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    *returnSize = n;
+
+    *returnColumnSizes = (int*)malloc(n * sizeof(int));
+
+    int** result = (int**)malloc(n * sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        (*returnColumnSizes)[i] = m;
+        result[i] = (int*)malloc(m * sizeof(int));
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+
+    return result;
+}
