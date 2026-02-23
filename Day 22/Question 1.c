dbc22d72320adf23ea7080b5a7139c277ef64ea0
Aparna Singh <aparnasingh29062006@gmail.com>
2026-02-23 19:21:26 +0530

    Add linked list node counting functionality
    Implement a function to count nodes in a linked list and create a linked list from user input.

---
+++ Day 22/Question 1.c
@@ -0,0 +1,45 @@
+/*Problem: Count Nodes in Linked List*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("%d", countNodes(head));
+
+    return 0;
+}
