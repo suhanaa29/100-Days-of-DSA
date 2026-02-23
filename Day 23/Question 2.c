09e6dfa58072a8fa1f06398af326fb4dd8791395
Aparna Singh <aparnasingh29062006@gmail.com>
2026-02-23 19:34:40 +0530

    Add function to detect cycle in linked list
    Implement Floyd's cycle detection algorithm for linked lists.

---
+++ Day 23/Question 2.c
@@ -0,0 +1,17 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.*/
+
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
