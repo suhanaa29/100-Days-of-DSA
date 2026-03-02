1564eb61aab9b769b3d0b3fb6e2f89d69eb08441
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-02 10:47:13 +0530

    Create Question 1.c

---
+++ Day 30/Question 1.c
@@ -0,0 +1,61 @@
+/*Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+struct Node* insertEnd(struct Node* head, int coeff, int exp) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = coeff;
+    newNode->exp = exp;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, coeff, exp;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        head = insertEnd(head, coeff, exp);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
