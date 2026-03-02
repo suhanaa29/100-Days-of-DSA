ff8cc1d055ae40b42c967f3aabde5d7ff01d6636
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-02 10:15:05 +0530

    Add linked list implementation for key deletion
    Implement linked list operations to delete the first occurrence of a key.

---
+++ Day 24/Question 1.c
@@ -0,0 +1,77 @@
+/*Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* deleteKey(struct Node* head, int key) {
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head;
+
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value, key;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    printList(head);
+
+    return 0;
+}
