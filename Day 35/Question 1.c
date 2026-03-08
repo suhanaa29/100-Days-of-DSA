0df55480eb1010933e879454694ea6845be24aab
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-08 09:54:34 +0530

    Create Question 1.c

---
+++ Day 35/Question 1.c
@@ -0,0 +1,54 @@
+/*Problem: Queue Using Array - Implement using linked list with dynamic memory allocation.*/
+
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int x)
+{
+    if (rear == MAX - 1)
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    rear++;
+    queue[rear] = x;
+}
+
+void display()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty");
+        return;
+    }
+
+    for (int i = front; i <= rear; i++)
+    {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main()
+{
+    int n, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
