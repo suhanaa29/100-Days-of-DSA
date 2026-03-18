1ae8fcbc9f712efea3a82d71f94ef90d17409e4e
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-18 14:40:26 +0530

    Create Question 1.c

---
+++ Day 42/Question 1.c
@@ -0,0 +1,51 @@
+/*Problem Statement:
+Given a queue of integers, reverse the queue using a stack*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int queue[MAX], front = 0, rear = -1;
+int stack[MAX], top = -1;
+
+void enqueue(int x) {
+    queue[++rear] = x;
+}
+
+int dequeue() {
+    return queue[front++];
+}
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    while (front <= rear)
+        push(dequeue());
+
+    front = 0;
+    rear = -1;
+
+    while (top >= 0)
+        enqueue(pop());
+
+    for (int i = front; i <= rear; i++)
+        printf("%d ", queue[i]);
+
+    return 0;
+}
