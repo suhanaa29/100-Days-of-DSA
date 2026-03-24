87a80b21797c5b49a1c40a34e9a4a498fbe0e23d
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:28:24 +0530

    Create Question 2.c

---
+++ Day 50/Question 2.c
@@ -0,0 +1,9 @@
+/*Search in BST*/
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (!root || root->val == val) return root;
+
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else
+        return searchBST(root->right, val);
+}
