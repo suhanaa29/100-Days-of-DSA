724a7627f045f32c381cb218628389f9f026e16c
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:24:17 +0530

    Create Question 2.c

---
+++ Day 49/Question 2.c
@@ -0,0 +1,17 @@
+/*Insert into BST*/
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (!root) {
+        struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        node->val = val;
+        node->left = node->right = NULL;
+        return node;
+    }
+
+    if (val < root->val)
+        root->left = insertIntoBST(root->left, val);
+    else
+        root->right = insertIntoBST(root->right, val);
+
+    return root;
+}
