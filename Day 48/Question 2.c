5ab0aa956a1621bba8cebcac4a861ddefdb489d1
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:20:47 +0530

    Create Question 2.c

---
+++ Day 48/Question 2.c
@@ -0,0 +1,12 @@
+/*Given the root of a binary tree, return the sum of all left leaves*/
+
+int sumOfLeftLeaves(struct TreeNode* root) {
+    if (!root) return 0;
+
+    int sum = 0;
+
+    if (root->left && !root->left->left && !root->left->right)
+        sum += root->left->val;
+
+    return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+}
