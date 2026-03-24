8cbef985fa99dffeedaf61f5d9f2db695287ae08
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:32:42 +0530

    Create Question 2.c

---
+++ Day 51/Question 2.c
@@ -0,0 +1,14 @@
+/*Given a binary search tree (BST), find the lowest common ancestor
+(LCA) node of two given nodes in the BST*/
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (!root) return NULL;
+
+    if (p->val < root->val && q->val < root->val)
+        return lowestCommonAncestor(root->left, p, q);
+
+    if (p->val > root->val && q->val > root->val)
+        return lowestCommonAncestor(root->right, p, q);
+
+    return root;
+}
