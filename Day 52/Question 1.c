58fe10d4cfcd98b2789c9e458a7f52050fd1a6c1
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:35:55 +0530

    Create Question 1.c

---
+++ Day 52/Question 1.c
@@ -0,0 +1,81 @@
+/*LCA in binary tree
+Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Tree*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* root = createNode(arr[0]);
+    struct TreeNode* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+struct TreeNode* LCA(struct TreeNode* root, int p, int q) {
+    if (!root) return NULL;
+
+    if (root->val == p || root->val == q)
+        return root;
+
+    struct TreeNode* left = LCA(root->left, p, q);
+    struct TreeNode* right = LCA(root->right, p, q);
+
+    if (left && right) return root;
+
+    return left ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    int p, q;
+    scanf("%d %d", &p, &q);
+
+    struct TreeNode* ans = LCA(root, p, q);
+
+    if (ans) printf("%d\n", ans->val);
+
+    return 0;
+}
