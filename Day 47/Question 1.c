556cc24c1bf39392b7d0c117ca936619c67e7eac
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-24 20:14:33 +0530

    Create Question 1.c

---
+++ Day 47/Question 1.c
@@ -0,0 +1,69 @@
+/*Problem: Height of Binary Tree*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* root = createNode(arr[0]);
+    struct TreeNode* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int height(struct TreeNode* root) {
+    if (!root) return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
