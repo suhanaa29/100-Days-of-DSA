2cb062e9818fd42bc2be98b007b104e1c82b5514
Aparna Singh <aparnasingh29062006@gmail.com>
2026-03-29 23:03:46 +0530

    Implement level order traversal for binary tree

---
+++ Daay 53/Question 2.c
@@ -0,0 +1,36 @@
+/*Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level)*/
+
+int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    if (!root) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int** result = (int**)malloc(2000 * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(2000 * sizeof(int));
+    *returnSize = 0;
+
+    struct TreeNode* queue[2000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        result[*returnSize] = (int*)malloc(size * sizeof(int));
+        (*returnColumnSizes)[*returnSize] = size;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* node = queue[front++];
+            result[*returnSize][i] = node->val;
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+
+        (*returnSize)++;
+    }
+
+    return result;
+}
