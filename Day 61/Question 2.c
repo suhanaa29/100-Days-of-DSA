913e7a1bb28b8702c78a21f54375d8d4b06a03ee
Aparna Singh <aparnasingh29062006@gmail.com>
2026-04-05 12:04:49 +0530

    Add function to find total number of provinces
    Implement DFS to count provinces in a graph.

---
+++ Day 61/Question 2.c
@@ -0,0 +1,26 @@
+/*Return the total number of provinces*/
+
+void dfs(int** isConnected, int n, int* visited, int i) {
+    for (int j = 0; j < n; j++) {
+        if (isConnected[i][j] == 1 && !visited[j]) {
+            visited[j] = 1;
+            dfs(isConnected, n, visited, j);
+        }
+    }
+}
+
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int n = isConnectedSize;
+    int visited[n];
+    for (int i = 0; i < n; i++) visited[i] = 0;
+    
+    int provinces = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            provinces++;
+            visited[i] = 1;
+            dfs(isConnected, n, visited, i);
+        }
+    }
+    return provinces;
+}
