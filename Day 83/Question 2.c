b192962ec7aa2aa1daac7f610f458ebcc824a077
Suhanaa <aparnasingh29062006@gmail.com>
2026-04-26 22:43:18 +0530

    Add search function for target in rotated array
    Implement binary search for rotated sorted array.

---
+++ Day 83/Question 2.c
@@ -0,0 +1,30 @@
+/*Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums*/
+
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
